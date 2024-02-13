6b0a26f9f899895803eb5ef498769716ef698807
Brayan Cantos <brayancantosm07ci@gmail.com>
2024-02-13 12:17:35 -0500

    🔧 chore(video_x.dart): add youtube getter to check if the video site is 'YouTube' 📝 docs(video_x.dart): add documentation for the youtube getter in the Video extension

--- lib/features/movie/presentation/extensions/video_x.dart
+++ lib/features/movie/presentation/extensions/video_x.dart
@@ -2,4 +2,6 @@ import 'package:flut_cinematic_domain/flut_cinematic_domain.dart';
 
 extension VideoX on Video {
   Uri get trailer => Uri.https('youtu.be', key);
+
+  bool get youtube => site == 'YouTube';
 }
