19821d1729f8b6abe00b98ca20f5d5c6d9464460
Brayan Cantos <brayancantosm07ci@gmail.com>
2024-02-13 12:01:38 -0500

    🚀 feat(extensions.dart): add video_x.dart extension file to export the VideoX extension 🎉 feat(video_x.dart): add VideoX extension to retrieve the trailer URL for a Video object

--- lib/features/movie/presentation/extensions/extensions.dart
+++ lib/features/movie/presentation/extensions/extensions.dart
@@ -1,2 +1,3 @@
 export 'duration_x.dart';
 export 'movie_x.dart';
+export 'video_x.dart';
---
+++ lib/features/movie/presentation/extensions/video_x.dart
@@ -0,0 +1,5 @@
+import 'package:flut_cinematic_domain/flut_cinematic_domain.dart';
+
+extension VideoX on Video {
+  Uri get trailer => Uri.https('youtu.be', key);
+}
