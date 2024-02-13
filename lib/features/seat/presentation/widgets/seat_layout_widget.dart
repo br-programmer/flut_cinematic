402d8ee2765b4b3962c5156e81a6efbb9efd3120
Brayan Cantos <brayancantosm07ci@gmail.com>
2024-02-13 12:34:41 -0500

    🔧 fix(seat_layout_widget.dart): wrap seat layout widget with InteractiveViewer to enable zooming and panning functionality

--- lib/features/seat/presentation/widgets/seat_layout_widget.dart
+++ lib/features/seat/presentation/widgets/seat_layout_widget.dart
@@ -14,32 +14,36 @@ class SeatLayoutWidget extends HookConsumerWidget {
         padding: edgeInsetsH4,
         child: FittedBox(
           fit: BoxFit.scaleDown,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: rows.mapList(
-              (row) {
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: row.items.mapList(
-                    (item) {
-                      final index = rows.indexOf(row);
-                      return item.status.child(
-                        index: index,
-                        onPressed: (value) {
-                          final letter = leters[index];
-                          final seat = '$letter${item.index + 1}';
-                          ref.read(ticketProvider.notifier).newSeat(seat);
-                          ref.read(seatProvider.notifier).update(
-                                indexR: item.index + 1,
-                                indexC: index,
-                                status: value,
-                              );
-                        },
-                      );
-                    },
-                  ),
-                );
-              },
+          child: InteractiveViewer(
+            minScale: 1,
+            maxScale: 2,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: rows.mapList(
+                (row) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: row.items.mapList(
+                      (item) {
+                        final index = rows.indexOf(row);
+                        return item.status.child(
+                          index: index,
+                          onPressed: (value) {
+                            final letter = leters[index];
+                            final seat = '$letter${item.index + 1}';
+                            ref.read(ticketProvider.notifier).newSeat(seat);
+                            ref.read(seatProvider.notifier).update(
+                                  indexR: item.index + 1,
+                                  indexC: index,
+                                  status: value,
+                                );
+                          },
+                        );
+                      },
+                    ),
+                  );
+                },
+              ),
             ),
           ),
         ),
