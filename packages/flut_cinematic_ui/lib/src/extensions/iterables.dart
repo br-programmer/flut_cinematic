4ff9209ce93de906e077d78c15adc42d355d729c
Brayan Cantos <brayancantosm07ci@gmail.com>
2024-02-13 12:17:47 -0500

    🚀 feat(iterables.dart): add whereList extension method to IterableX
    The `whereList` extension method is added to the `IterableX` class. This method provides a shortcut for filtering elements in an iterable and converting the result to a list. It takes a predicate function as an argument and returns a list of elements that satisfy the predicate.

--- packages/flut_cinematic_ui/lib/src/extensions/iterables.dart
+++ packages/flut_cinematic_ui/lib/src/extensions/iterables.dart
@@ -5,4 +5,9 @@ extension IterableX<E> on Iterable<E> {
   List<T> mapList<T>(T Function(E element) f) {
     return map(f).toList();
   }
+
+  /// Shortcut for `where((E e) => e).toList()`.
+  List<E> whereList<T>(bool Function(E element) f) {
+    return where(f).toList();
+  }
 }
