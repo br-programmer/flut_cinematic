a6f224d299925debb6f5008031bb069fdf921953
Brayan Cantos <brayancantosm07ci@gmail.com>
2024-02-13 12:29:20 -0500

    🔀 chore(core): reorganize core files and add services module
    📦 refactor(core.dart): add 'services' module to the core package to group related service classes
    
    📦 feat(services.dart): add 'url_launcher_service.dart' to the services module to provide URL launching functionality
    
    📦 feat(services.dart): add 'urlLaunch' provider to the Services class in the injector module to enable dependency injection for the UrlLauncherService

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -2,3 +2,4 @@ export 'app/app.dart';
 export 'injector/injector.dart';
 export 'presentation/presentation.dart';
 export 'router/router.dart';
+export 'services/services.dart';
--- lib/core/injector/services.dart
+++ lib/core/injector/services.dart
@@ -1,3 +1,4 @@
+import 'package:flut_cinematic/core/core.dart';
 import 'package:flut_cinematic_firebase/flut_cinematic_firebase.dart';
 import 'package:riverpod/riverpod.dart';
 
@@ -6,5 +7,9 @@ class Services {
 
   static final auth = Provider<FlutCinematicAuthService>(
     (ref) => FlutCinematicAuthService(),
+  );
+
+  static final urlLaunch = Provider<UrlLauncherService>(
+    (ref) => const UrlLauncherService(),
   );
 }
