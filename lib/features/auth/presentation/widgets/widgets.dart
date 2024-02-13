b3d6d9893be76f2da0204e1251934fba7bfd94c3
Brayan Cantos <brayancantosm07ci@gmail.com>
2024-02-13 14:45:49 -0500

    ✨ feat(auth): add login widgets for Apple Sign In, Email Input, Google Sign In, Header, Password Input, and Sign In Button to improve user authentication experience

---
+++ lib/features/auth/presentation/widgets/widgets.dart
@@ -0,0 +1,6 @@
+export 'login_apple_sign_in.dart';
+export 'login_email_imput.dart';
+export 'login_google_sign_in.dart';
+export 'login_header.dart';
+export 'login_password_imput.dart';
+export 'login_sign_in_button.dart';
